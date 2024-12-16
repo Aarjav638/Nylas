63bab13573af67c8cd8ed68179c15c4982f455fc
Aarjav Jain <anshjain638@gmail.com>
2024-12-16 11:40:05 +0530

    addded boilerPlate Code

---
+++ index.js
@@ -0,0 +1,30 @@
+import Express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import 'dotenv/config'
+const app = Express();
+app.use(cors());
+app.use(Express.json());
+
+app.use(morgan('dev'));
+
+
+const nylasConfig = {
+    clientId: process.env.CLIENT_ID,
+    apiKey: process.env.API_KEY,
+    apiUri: process.env.API_URI,
+}
+
+console.log(nylasConfig);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    }
+);
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
